fcdab251b144a5158deb4b9f4f4b28c2fb391c3d
Nico Rossi <nrossi@itba.edu.ar>
2026-01-31 00:13:03 -0300

    [Infra] S3 Bucket module

---
+++ deploy/modules/storage/main.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  force_destroy = var.environment == "local"
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning_v2" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.this.arn
+}
+
+output "bucket_id" {
+  value = aws_s3_bucket.this.id
+}
