5549c15866db3cff412e7bf36bc3ed8cc9f4f42a
Nico Rossi <nrossi@itba.edu.ar>
2026-01-31 01:37:55 -0300

    [Infra] VPC and Subnet default discovery

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,3 +1,14 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 module "storage" {
   source      = "./modules/storage"
   bucket_name = "milo-raw-ingest-${var.environment}"
@@ -14,7 +25,7 @@ module "messaging" {
 module "database" {
   source      = "./modules/database"
   environment = var.environment
-  vpc_id      = var.vpc_id
+  vpc_id      = data.aws_vpc.default.id
   db_name     = var.db_name
   db_username = var.db_username
   db_password = var.db_password
@@ -29,7 +40,7 @@ module "iam" {
 }
 
 module "ingest_service" {
-  source             = "./modules/ecs-service"
+  source             = "./modules/ecs"
   environment        = var.environment
   aws_region         = var.aws_region
   container_image    = "milo/doclin:latest"
@@ -38,6 +49,6 @@ module "ingest_service" {
   db_url             = module.database.db_endpoint
   sq_queue_url       = module.messaging.queue_url
   s3_bucket_id       = module.storage.bucket_id
-  subnet_ids         = [var.subnet_id]
+  subnet_ids         = data.aws_subnets.default.ids
   security_group_id  = module.database.db_security_group_id # Shared for PoC
 }
